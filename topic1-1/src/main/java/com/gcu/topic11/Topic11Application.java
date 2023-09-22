bb64af401d6dfb25f7686bc3b6e0785c2055a6ea
mhampton2003 <74636177+mhampton2003@users.noreply.github.com>
2023-09-21 22:12:03 -0700

    initial upload

---
+++ topic1-1/src/main/java/com/gcu/topic11/Topic11Application.java
@@ -0,0 +1,14 @@
+package com.gcu.topic11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Topic11Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello from my Spring Boot application");
+		SpringApplication.run(Topic11Application.class, args);
+	}
+
+}
